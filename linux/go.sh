76c4ee238d7071ffdd0058cbab1c4e4e94b2536c
FiveTech Software <antonio.fivetech@gmail.com>
2019-05-20 00:48:24 +0200

    Update go.sh

--- linux/go.sh
+++ linux/go.sh
@@ -2,4 +2,4 @@ sudo apt-get update -y
 sudo apt-get install apache2 -y
 sudo cp ./mod_harbour/linux/Ubuntu 18.04/mod_harbour.so /usr/lib/apache2/modules
 sudo cp ./mod_harbour/linux/Ubuntu 18.04/libharbour.so.3.2.0 /var/www/html
-
+sudo cp ./mod_harbour/linux/test.prg /var/www/html
