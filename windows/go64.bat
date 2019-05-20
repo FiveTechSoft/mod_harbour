491768aea109676a7096d80629f04a996bc3c7e9
FiveTech Software <antonio.fivetech@gmail.com>
2019-05-20 07:40:06 +0200

    Delete go64.bat

--- windows/go64.bat
+++
@@ -1,9 +0,0 @@
-c:\harbour\bin\win\msvc64\harbour -n main.prg
-
-rem call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-
-cl -TC -LD -Ic:\harbour\include -Ic:\Apache24\include mod_harbour.c main.c @libs.txt -link -out:mod_harbour.so
-
-copy mod_harbour.so c:\Apache24\modules\mod_harbour.so 
