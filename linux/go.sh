3b1adc60985c6ca7d3028442afa834074f952cbc
FiveTech Software <antonio.fivetech@gmail.com>
2019-05-28 06:27:12 +0200

    Update go.sh

--- linux/go.sh
+++ linux/go.sh
@@ -1,7 +1,7 @@
 sudo apt-get update -y
 sudo apt-get install apache2 -y
-sudo cp ./mod_harbour/linux/"Ubuntu 18.04"/20190524/mod_harbour.so /usr/lib/apache2/modules
-sudo cp ./mod_harbour/linux/"Ubuntu 18.04"/20190524/libharbour.so.3.2.0 /var/www/html
+sudo cp ./mod_harbour/linux/"Ubuntu 18.04"/20190528/mod_harbour.so /usr/lib/apache2/modules
+sudo cp ./mod_harbour/linux/"Ubuntu 18.04"/20190528/libharbour.so.3.2.0 /var/www/html
 echo 'LoadModule harbour_module /usr/lib/apache2/modules/mod_harbour.so' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
 echo '<FilesMatch "\.(prg)$">' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
 echo 'SetHandler harbour' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
