9346c0d7657f4437bc7d556895a50b2346ca32f2
FiveTech Software <antonio.fivetech@gmail.com>
2019-05-29 16:47:00 +0200

    Update build.sh

--- linux/build.sh
+++ linux/build.sh
@@ -19,6 +19,8 @@ export HB_BUILD_CONTRIBS=no
 make
 cp ./lib/linux/gcc/libharbour.so.3.2.0 /var/www/html
 cp ./lib/linux/gcc/libharbour.so.3.2.0 ~/mod_harbour/linux/"Ubuntu 18.04"
+cp ~/mod_harbour/samples/* /var/www/html
+sudo apachectl restart
 cd ~/mod_harbour
 git pull
 git commit /linux/"Ubuntu 18.04"/libharbour.so.3.2.0 -m"new build"
