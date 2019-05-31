b40a508793ed78bb5b93aeb378f6e455c88886ce
FiveTech Software <antonio.fivetech@gmail.com>
2019-05-31 18:49:00 +0200

    Update go.sh

--- linux/go.sh
+++ linux/go.sh
@@ -7,7 +7,7 @@ cd /var/www/html
 sudo ln -sf $cwd/linux/"Ubuntu 18.04"/libharbour.so.3.2.0 libharbour.so.3.2.0
 sudo ln -sf $cwd/samples modharbour_samples
 cd /usr/lib/apache2/modules
-sudo ln -s $cwd/linux/"Ubuntu 18.04"/mod_harbour.so mod_harbour.so
+sudo ln -sf $cwd/linux/"Ubuntu 18.04"/mod_harbour.so mod_harbour.so
 cd $cwd
 echo 'LoadModule harbour_module /usr/lib/apache2/modules/mod_harbour.so' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
 echo '<FilesMatch "\.(prg)$">' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
