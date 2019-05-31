cbbef4a70d40febab976922a6c37b4ab58d36dc4
FiveTech Software <antonio.fivetech@gmail.com>
2019-05-31 18:10:28 +0200

    Update go.sh

--- linux/go.sh
+++ linux/go.sh
@@ -1,5 +1,6 @@
 sudo apt-get update -y
 sudo apt-get install apache2 -y
+sudo apt-get install mysql-server -y
 sudo cp ./mod_harbour/linux/"Ubuntu 18.04"/mod_harbour.so /usr/lib/apache2/modules
 sudo cp ./mod_harbour/linux/"Ubuntu 18.04"/libharbour.so.3.2.0 /var/www/html
 echo 'LoadModule harbour_module /usr/lib/apache2/modules/mod_harbour.so' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
