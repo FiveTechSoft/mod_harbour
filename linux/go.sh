e0950ad9fbfcb3cce7bba5a8d251556d60f982aa
FiveTech Software <antonio.fivetech@gmail.com>
2019-05-31 18:26:05 +0200

    Update go.sh

--- linux/go.sh
+++ linux/go.sh
@@ -2,12 +2,15 @@ sudo apt-get update -y
 sudo apt-get install apache2 -y
 sudo apt-get install mysql-server -y
 sudo apt-get install libmysqlclient-dev -y
-sudo cp ./mod_harbour/linux/"Ubuntu 18.04"/mod_harbour.so /usr/lib/apache2/modules
-sudo cp ./mod_harbour/linux/"Ubuntu 18.04"/libharbour.so.3.2.0 /var/www/html
+cwd=$(pwd)
+cd /var/www/html
+sudo ln -sf $cwd/linux/"Ubuntu 18.04"/mod_harbour.so mod_harbour.so
+sudo ln -sf $cwd/samples modharbour_samples
+cd /usr/lib/apache2/modules
+sudo ln -s $cwd/linux/"Ubuntu 18.04"/mod_harbour.so mod_harbour.so
+cd $cwd
 echo 'LoadModule harbour_module /usr/lib/apache2/modules/mod_harbour.so' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
 echo '<FilesMatch "\.(prg)$">' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
 echo 'SetHandler harbour' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
 echo '</FilesMatch>' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
-sudo cp ./mod_harbour/samples/test.prg /var/www/html
-sudo cp ./mod_harbour/samples/info.prg /var/www/html
 sudo apachectl restart
