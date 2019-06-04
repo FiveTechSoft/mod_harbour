cba7f058fc0db22e717213490141f93a83948eac
FiveTech Software <antonio.fivetech@gmail.com>
2019-06-04 17:39:39 +0200

    Delete go64.bat

--- windows_new/win64/go64.bat
+++
@@ -1,5 +0,0 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-HB_BUILD_MODE=c
-HB_USER_PRGFLAGS=-l-
-HB_BUILD_CONTRIBS=no
-win-make.exe
