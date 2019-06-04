38749115b24da4018d4fda813ffed88f20bf238a
FiveTech Software <antonio.fivetech@gmail.com>
2019-06-04 21:03:52 +0200

    Update build.sh

--- linux/build.sh
+++ linux/build.sh
@@ -30,14 +30,14 @@ export HB_USER_PRGFLAGS=-l-
 export HB_BUILD_CONTRIBS=no
 make
 cd /var/www/html
-ln -sf ~/harbour_for_modharbour/lib/linux/gcc/libharbour.so.3.2.0 libharbour.so.3.2.0
-ln -sf ~/mod_harbour/samples mod_harbour_samples
+sudo ln -sf ~/harbour_for_modharbour/lib/linux/gcc/libharbour.so.3.2.0 libharbour.so.3.2.0
+sudo ln -sf ~/mod_harbour/samples mod_harbour_samples
 cd ~/mod_harbour/linux/"Ubuntu 18.04"
 ln -sf ~/harbour_for_modharbour/lib/linux/gcc/libharbour.so.3.2.0 libharbour.so.3.2.0
 cd /usr/lib/apache2/modules
-ln -sf ~/mod_harbour/linux/"Ubuntu 18.04"/mod_harbour.so mod_harbour.so
+sudo ln -sf ~/mod_harbour/linux/"Ubuntu 18.04"/mod_harbour.so mod_harbour.so
 sudo apachectl restart
 cd ~/mod_harbour
 git pull
-git commit /linux/"Ubuntu 18.04"/libharbour.so.3.2.0 -m"new build"
-git push
+#git commit /linux/"Ubuntu 18.04"/libharbour.so.3.2.0 -m"new build"
+#git push
