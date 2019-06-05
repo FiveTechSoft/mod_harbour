e63df0517a4780cc8874c161cdd7413a48f12614
FiveTech Software <antonio.fivetech@gmail.com>
2019-06-05 19:59:57 +0200

    Update mod_harbour.c

--- windows_new/mod_harbour.c
+++ windows_new/mod_harbour.c
@@ -183,7 +183,8 @@ static int harbour_handler( request_rec * r )
    r->content_type = "text/html";
    _r = r;
 
-   lib_harbour = LoadLibrary( "c:\\Apache24\\htdocs\\libharbour.dll" );
+   if( ( lib_harbour = LoadLibrary( ap_getenv( "LIBHARBOUR" ) ) == NULL ) 
+      lib_harbour = LoadLibrary( "c:\\Apache24\\htdocs\\libharbour.dll" );
 
    if( lib_harbour == NULL )
    {
