66b0ebbc8c0b7bb9cdb7a8e7d10ed6e8b5b213da
Antonio Linares <antonio.fivetech@gmail.com>
2019-06-09 21:36:17 +0200

    some changes

--- contribs/go.bat
+++ contribs/go.bat
@@ -1,2 +1,2 @@
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-link.exe /MACHINE:amd64 -nxcompat -dynamicbase -nologo -dll -subsystem:console -libpath:c:/harbour_for_modharbour64/lib/win/msvc64 -def:harbour.def -nodefaultlib:libcmt.lib -out:"harbour-32-x64.dll" @libs.txt msvcrt.lib winmm.lib kernel32.lib user32.lib ws2_32.lib iphlpapi.lib advapi32.lib gdi32.lib
+link.exe /MACHINE:amd64 -nxcompat -dynamicbase -nologo -dll -subsystem:console -libpath:c:/harbour_for_modharbour64/lib/win/msvc64 -def:harbour.def -nodefaultlib:libcmt.lib -out:"libharbour.dll" @libs.txt @windows.txt
