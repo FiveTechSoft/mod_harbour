207779349a9af8040d9c26562b8972fd60563470
FiveTech Software <antonio.fivetech@gmail.com>
2019-06-10 07:26:54 +0200

    Update go.sh

--- contribs/linux/go.sh
+++ contribs/linux/go.sh
@@ -1,1 +1,1 @@
-gcc  -shared -L../../../../../lib/linux/gcc -Wl,-soname,libharbour.so.3.2  -o libharbour.so.3.2.0
+gcc -shared -L~/harbour/lib/linux/gcc -Wl,-soname,libharbour.so.3.2  -o libharbour.so.3.2.0
