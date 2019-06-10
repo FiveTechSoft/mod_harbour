56724af52a3215108ad31f62724b53b0d6e76ff3
Antonio Linares <antonio.fivetech@gmail.com>
2019-06-10 08:06:55 +0200

    support for Linux

--- contribs/linux/go.sh
+++ contribs/linux/go.sh
@@ -1,1 +1,1 @@
-gcc -shared -L~/harbour/lib/linux/gcc -Wl,-soname,libharbour.so.3.2  -o libharbour.so.3.2.0 -ldl -lpthread -lm -lrt
+gcc -shared -Wl,-soname,libharbour.so.3.2 -olibharbour.so.3.2.0 -L/home/anto/harbour_for_modharbour/lib/linux/gcc @libs.txt -ldl -lpthread -lm -lrt
