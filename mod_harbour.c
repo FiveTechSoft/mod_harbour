c9a3c9bac649056406a0ad1c7968e3a131071afb
FiveTech Software <antonio.fivetech@gmail.com>
2019-06-10 19:56:12 +0200

    Update mod_harbour.c

--- mod_harbour.c
+++ mod_harbour.c
@@ -188,6 +188,7 @@ static int harbour_handler( request_rec * r )
    {
       ap_parse_form_data( r, NULL, &POST_pairs, -1, HUGE_STRING_LEN );
       ap_add_common_vars( r );
+      ap_add_cgi_vars( r );
    
       #ifdef _MSC_VER
          ( ( FARPROC ) _hb_apache ) = GetProcAddress( lib_harbour, "hb_apache" );
