1b7ef278210656da23a32f03a26436013ce22e4f
Antonio Linares <antonio.fivetech@gmail.com>
2019-06-10 10:38:09 +0200

    minor fix

--- contribs/linux/go.sh
+++ contribs/linux/go.sh
@@ -1,1 +1,1 @@
-gcc -shared -olibharbour.so.3.2.0 -L/home/anto/harbour_for_modharbour/lib/linux/gcc -Wl,--whole-archive @libs.txt -Wl,--no-whole-archive -ldl -lpthread -lm -lrt
+gcc -shared -olibharbour.so.3.2.0 -L$HOME/harbour_for_modharbour/lib/linux/gcc -Wl,--whole-archive @libs.txt -Wl,--no-whole-archive -ldl -lpthread -lm -lrt
