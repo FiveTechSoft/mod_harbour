0dec95e6d3ea7115fe24f2c0fa568ac659dfe3f1
anto <anto@DESKTOP-QSSD1BK.home>
2019-07-05 14:53:10 +0200

    install.sh for OSX

--- osx/go.sh
+++
@@ -1,5 +0,0 @@
-cp mod_harbour.so /usr/local/lib/httpd/modules
-cd /usr/local/var/www
-sudo ln -sf /Users/$USER/mod_harbour/osx/libharbour.3.2.0.dylib libharbour.3.2.0.dylib
-sudo ln -sf /Users/anto/mod_harbour/samples modharbour_samples
-sudo apachectl restart
---
+++ osx/install.sh
@@ -0,0 +1,13 @@
+echo Installing mod_harbour for OSX...
+sudo mkdir -p /usr/local/httpd/modules
+cd /usr/local/httpd/modules
+sudo ln -sf /Users/$USER/mod_harbour/osx/mod_harbour.so mod_harbour.so
+cd /Library/WebServer/Documents
+sudo ln -sf /Users/$USER/mod_harbour/osx/libharbour.3.2.0.dylib libharbour.3.2.0.dylib
+sudo ln -sf /Users/$USER/mod_harbour/samples modharbour_samples
+echo 'LoadModule harbour_module /usr/local/httpd/modules/mod_harbour.so' | sudo tee -a /private/etc/apache2/httpd.conf > /dev/null
+echo '<FilesMatch "\.(prg|hrb)$">' | sudo tee -a /private/etc/apache2/httpd.conf > /dev/null
+echo 'SetHandler harbour' | sudo tee -a /private/etc/apache2/httpd.conf > /dev/null
+echo '</FilesMatch>' | sudo tee -a /private/etc/apache2/httpd.conf > /dev/null
+sudo apachectl restart
+echo Please go to localhost/modharbour_samples fron your web browser
