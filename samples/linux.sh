b61f00dae34449ddeab42a9ef45df7c99bc84294
anto <anto@MacBook-Air-de-Usuario.local>
2019-07-10 07:45:04 +0200

    data permits

---
+++ samples/linux.sh
@@ -0,0 +1,17 @@
+sudo chown -R www-data:www-data data
+cd apps/honey
+sudo chown -R www-data:www-data data
+cd ../..
+cd apps/mvc
+sudo chown -R www-data:www-data data
+cd ../..
+cd apps/mvc_notes
+sudo chown -R www-data:www-data data
+cd ../..
+cd eshop
+sudo chown -R www-data:www-data data
+cd ..
+cd genesis
+sudo chown -R www-data:www-data data
+cd ..
+sudo chown -R www-data:www-data machine
