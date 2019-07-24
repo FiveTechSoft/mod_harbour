563c74bc4635fb22dba0c6e4ac689ff74808bba4
anto <anto@antos-MacBook-Pro.local>
2019-07-24 05:27:59 -0700

    more on OSX setup

--- osx/install.sh
+++ osx/install.sh
@@ -9,7 +9,8 @@ echo 'LoadModule harbour_module /usr/local/httpd/modules/mod_harbour.so' | sudo 
 echo '<FilesMatch "\.(prg|hrb)$">' | sudo tee -a /private/etc/apache2/httpd.conf > /dev/null
 echo 'SetHandler harbour' | sudo tee -a /private/etc/apache2/httpd.conf > /dev/null
 echo '</FilesMatch>' | sudo tee -a /private/etc/apache2/httpd.conf > /dev/null
+echo 'Options Indexes FollowSymLinks Multiviews'| sudo tee -a /private/etc/apache2/httpd.conf > /dev/null
 cd /Users/$USER/mod_harbour
 sudo chown -R _www:_www samples
 sudo apachectl restart
-echo Please go to localhost/modharbour_samples fron your web browser
+echo Please go to localhost/modharbour_samples from your web browser
