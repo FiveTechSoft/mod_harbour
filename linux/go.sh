8f3188040b7064b0f90df1c01fd6f47ebf5e7c14
FiveTech Software <antonio.fivetech@gmail.com>
2019-08-14 09:35:54 +0200

    Update go.sh

--- linux/go.sh
+++ linux/go.sh
@@ -5,13 +5,6 @@ sudo apt-get install libmysqlclient-dev -y
 cd /var/www/html
 sudo ln -sf ~/mod_harbour/linux/"Ubuntu 18.04"/libharbour.so.3.2.0 libharbour.so.3.2.0
 sudo ln -sf ~/mod_harbour/samples modharbour_samples
-cd ..
-sudo mkdir test
-sudo cp ~/mod_harbour/samples/customer.dbf ./test
-sudo chown -R www-data:www-data test
-rm ~/mod_harbour/samples/customer.dbf
-sudo chown -R www-data:www-data ~/mod_harbour/samples/apps/honey/data
-sudo chown -R www-data:www-data ~/mod_harbour/samples/eshop/data
 cd /usr/lib/apache2/modules
 sudo ln -sf ~/mod_harbour/linux/"Ubuntu 18.04"/mod_harbour.so mod_harbour.so
 cd ~
