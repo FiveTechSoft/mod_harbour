f6ba6a47042ac267701c8c95b13bc3db9a3f1fcc
Antonio <antonio.fivetech@gmail.com>
2020-02-18 16:48:28 +0100

    same mod.hbp file for 32 and 64 bits

--- hbmk2/windows/go32.bat
+++ hbmk2/windows/go32.bat
@@ -1,2 +1,2 @@
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
-c:\harbour\bin\win\msvc\hbmk2 modharbour.hbp -comp=msvc -trace
+c:\harbour\bin\win\msvc\hbmk2 modharbour.hbp -comp=msvc -Lc:\Apache24\lib32
