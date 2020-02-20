d28ef525b08ec312bd84bc52338019598e312861
FiveTech Software <antonio.fivetech@gmail.com>
2020-02-20 14:16:34 +0100

    Update go32.bat

--- hbmk2/windows/go32.bat
+++ hbmk2/windows/go32.bat
@@ -1,2 +1,4 @@
+@set oldpath=%path%
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 c:\harbour\bin\win\msvc\hbmk2 modharbour.hbp -comp=msvc -Lc:\Apache24\lib32
+@set path=%oldpath%
