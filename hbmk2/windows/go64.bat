d3b691b9e7b77e1535162dca694c5e2be68bf841
FiveTech Software <antonio.fivetech@gmail.com>
2020-02-20 14:17:11 +0100

    Update go64.bat

--- hbmk2/windows/go64.bat
+++ hbmk2/windows/go64.bat
@@ -1,2 +1,4 @@
+@set oldpath=%path%
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 c:\harbour\bin\win\msvc64\hbmk2 modharbour.hbp -comp=msvc64 -Lc:\Apache24\lib
+@set path=%oldpath%
