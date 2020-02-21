f218913346c2ba1a0bafa39f811e2f83f43ead09
FiveTech Software <antonio.fivetech@gmail.com>
2020-02-21 10:02:00 +0100

    Delete harbour64.bat

--- windows/win64/harbour64.bat
+++
@@ -1,5 +0,0 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-set HB_BUILD_MODE=c
-set HB_USER_PRGFLAGS=-l-
-set HB_BUILD_CONTRIBS=no
-win-make.exe
