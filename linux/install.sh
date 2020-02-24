be28801cc5ad07f505060b51848e0be550de5e86
FiveTech Software <antonio.fivetech@gmail.com>
2020-02-24 07:20:44 +0100

    Create install.sh

---
+++ linux/install.sh
@@ -0,0 +1,15 @@
+sudo apt-get update -y
+sudo apt-get install apache2 -y
+sudo apt-get install mysql-server -y
+sudo apt-get install libmysqlclient-dev -y
+cd /var/www/html
+sudo ln -sf ~/mod_harbour/linux/"Ubuntu 18.04"/libharbour.so.3.2.0 libharbour.so.3.2.0
+sudo ln -sf ~/mod_harbour/samples modharbour_samples
+cd /usr/lib/apache2/modules
+sudo ln -sf ~/mod_harbour/linux/"Ubuntu 18.04"/mod_harbour.so mod_harbour.so
+cd ~
+echo 'LoadModule harbour_module /usr/lib/apache2/modules/mod_harbour.so' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
+echo '<FilesMatch "\.(prg|hrb)$">' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
+echo 'SetHandler harbour' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
+echo '</FilesMatch>' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
+sudo apachectl restart
