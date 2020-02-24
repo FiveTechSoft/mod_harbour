621fff30e6c98c6b3ef98ae2a2d033e28da47812
FiveTech Software <antonio.fivetech@gmail.com>
2020-02-24 07:06:20 +0100

    Delete go.bat

--- contribs/windows/pdf32+xhb+hbwin+hbct/go.bat
+++
@@ -1,2 +0,0 @@
-call "%ProgramFiles%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
-link.exe /MACHINE:x86 -nxcompat -dynamicbase -nologo -dll -subsystem:console -libpath:c:/harbour_for_modharbour/lib/win/msvc -def:harbour.def -nodefaultlib:libcmt.lib -out:"libharbour.dll" @libs.txt @windows.txt
