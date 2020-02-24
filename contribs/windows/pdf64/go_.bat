3d791aefc4ce7a8ebcff65b32148d0bbc5ac2c18
FiveTech Software <antonio.fivetech@gmail.com>
2020-02-24 07:10:17 +0100

    Delete go_.bat

--- contribs/windows/pdf64/go_.bat
+++
@@ -1,6 +0,0 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-set HB_BUILD_MODE=c
-set HB_USER_PRGFLAGS=-l-
-set HB_WITH_LIBHARU=c:\software\libharu
-set HB_BUILD_3RDEXT=yes
-c:\harbour\bin\hbmk2 libhpdf
