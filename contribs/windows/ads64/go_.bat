f428811d71ea3f2d6efe4e6ee2eba787949323bd
FiveTech Software <antonio.fivetech@gmail.com>
2020-02-24 07:04:28 +0100

    Delete go_.bat

--- contribs/windows/ads64/go_.bat
+++
@@ -1,5 +0,0 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-set HB_BUILD_MODE=c
-set HB_USER_PRGFLAGS=-l-
-set HB_WITH_ADS=c:\Program Files\Advantage 11.10\acesdk\
-c:\harbour\bin\hbmk2 rddads
