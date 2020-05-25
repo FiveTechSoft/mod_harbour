938b93f1bc1ba4f380ea280f8ed771a4b631850e
FiveTech Software <antonio.fivetech@gmail.com>
2020-05-25 17:01:06 +0200

    Update console.c

--- source/console.c
+++ source/console.c
@@ -1,3 +1,11 @@
+/*
+**  console.c -- Apache harbour module
+**
+** (c) FiveTech Software SL, 2019-2020
+** Developed by Antonio Linares alinares@fivetechsoft.com
+** MIT license https://github.com/FiveTechSoft/mod_harbour/blob/master/LICENSE
+*/
+
 #include <hbapi.h>
 
 //----------------------------------------------------------------//
