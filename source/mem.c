eff8aea5e33b04c38af1cbdbd05c872066c6bcb0
FiveTech Software <antonio.fivetech@gmail.com>
2020-05-26 23:21:44 +0200

    Update mem.c

--- source/mem.c
+++ source/mem.c
@@ -59,18 +59,16 @@ HB_FUNC( MREAD )
 
 #define ignore_result(x) if (x){}
 
-static int iLen;
+#define MEMSIZE 100
 
 HB_FUNC( MWRITE )
 {
    void * bytes;
    int fd = shm_open( ( char * ) hb_parc( 1 ), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR );
 
-   // shm_unlink( hb_parc( 1 ) );
-   ignore_result( ftruncate( fd, iLen = hb_parclen( 2 ) ) );
-   bytes = mmap( NULL, hb_parclen( 2 ), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0 );
+   ignore_result( ftruncate( fd, MEMSIZE ) );
+   bytes = mmap( NULL, MEMSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0 );
    memcpy( bytes, hb_parc( 2 ), hb_parclen( 2 ) );
-   // close( fd );
 }
 
 HB_FUNC( MREAD )
@@ -78,9 +76,8 @@ HB_FUNC( MREAD )
    void * bytes;
    int fd = shm_open( ( char * ) hb_parc( 1 ), O_RDONLY, S_IRUSR | S_IWUSR );
    
-   bytes = mmap( NULL, iLen, PROT_READ, MAP_SHARED, fd, 0 );
+   bytes = mmap( NULL, MEMSIZE, PROT_READ, MAP_SHARED, fd, 0 );
    hb_retc( bytes );
-   // close( fd );
 }
 
 #endif
