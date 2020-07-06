36c80defb2bd8ed5a9a24f44a1fbc2bb2b4c8116
FiveTech Software <antonio.fivetech@gmail.com>
2020-07-06 10:34:44 +0200

    Add files via upload

---
+++ viewer/win32/go32.bat
@@ -0,0 +1,5 @@
+@set oldpath=%Path%
+@set oldinclude=%INCLUDE%
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+cl.exe main2.cc /EHsc /std:c++17 -I./script c:\webview\script\microsoft.web.webview2.0.9.488\build\native\x86\WebView2Loader.dll.lib -Fe:viewer.exe
+@set INCLUDE=%oldinclude%
