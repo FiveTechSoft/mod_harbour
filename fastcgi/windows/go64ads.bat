359a8ec09baf96c5db0cb772a388f7691936664d
FiveTech Software <antonio.fivetech@gmail.com>
2020-12-31 19:37:17 +0100

    Update go64ads.bat

--- fastcgi/windows/go64ads.bat
+++ fastcgi/windows/go64ads.bat
@@ -1,6 +1,6 @@
 @set oldpath=%path%
 @set oldinclude=%INCLUDE%
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-c:\harbour\bin\win\msvc64\hbmk2 modharbour.hbp -comp=msvc64 -hbx=modharbour.hbx -dHB_WITH_ADS="c:\Program Files\Advantage 11.10\acesdk\" rddads.hbc
+c:\harbour\bin\win\msvc64\hbmk2 modharbour.hbp -comp=msvc64 -hbx=modharbour.hbx -dHB_WITH_ADS -dHB_WITH_ADS="c:\Program Files\Advantage 11.10\acesdk\" rddads.hbc
 @set path=%oldpath%
 @set INCLUDE=%oldinclude%
