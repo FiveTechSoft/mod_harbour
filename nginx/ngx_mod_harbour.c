216a381d6832ff5b4edd7dbabfbd8bc19d7e43db
FiveTech Software <antonio.fivetech@gmail.com>
2021-05-09 20:20:18 +0200

    Update ngx_mod_harbour.c

--- nginx/ngx_mod_harbour.c
+++ nginx/ngx_mod_harbour.c
@@ -1,1 +1,18 @@
-// empty file
+#include <ngx_config.h>
+#include <ngx_core.h>
+
+extern ngx_module_t  ngx_mod_harbour;
+
+ngx_module_t *ngx_modules[] = {
+    &ngx_mod_harbour,
+    NULL
+};
+
+char *ngx_module_names[] = {
+    "ngx_mod_harbour",
+    NULL
+};
+
+char *ngx_module_order[] = {
+    NULL
+};
