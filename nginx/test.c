fc321f63a4bd2f57b5ce04823c27d293d34f9974
FiveTech Software <antonio.fivetech@gmail.com>
2021-05-16 14:03:40 +0200

    Create test.c

---
+++ nginx/test.c
@@ -0,0 +1,11 @@
+// build it doing: gcc test.c -ldl -otest 
+
+#include <stdio.h>
+   #include <dlfcn.h>
+
+int main()
+{
+   dlopen( "./ngx_mod_harbour.so", RTLD_NOW );
+   printf( "%s/n", dlerror() );
+   return 0;
+}
