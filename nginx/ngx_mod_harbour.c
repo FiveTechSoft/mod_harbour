282462d92edbe8d398affd8df6501713caac1fc6
Antonio Linares <antonio.fivetech@gmail.com>
2021-05-17 17:36:11 +0200

    loading libharbour.dll

--- nginx/ngx_mod_harbour.c
+++ nginx/ngx_mod_harbour.c
@@ -84,13 +84,83 @@ int mh_rputs( ngx_http_request_t * r, const char * szText )
    return ngx_http_output_filter( r, &out );
 }
 
+int CopyFile( const char * from, const char * to, int iOverWrite )
+{
+    int fd_to, fd_from;
+    char buf[ 4096 ];
+    ssize_t nread;
+    int saved_errno;
+
+    iOverWrite = iOverWrite;
+
+    fd_from = open( from, O_RDONLY );
+    if( fd_from < 0 )
+        return -1;
+
+    fd_to = open( to, O_WRONLY | O_CREAT | O_EXCL, 0666 );
+    if( fd_to < 0 )
+        goto out_error;
+
+    while( nread = read( fd_from, buf, sizeof buf ), nread > 0 )
+    {
+        char * out_ptr = buf;
+        ssize_t nwritten;
+
+        do {
+            nwritten = write( fd_to, out_ptr, nread );
+
+            if( nwritten >= 0 )
+            {
+                nread -= nwritten;
+                out_ptr += nwritten;
+            }
+            else if( errno != EINTR )
+            {
+                goto out_error;
+            }
+        } while( nread > 0 );
+    }
+
+    if( nread == 0 )
+    {
+        if( close( fd_to ) < 0 )
+        {
+            fd_to = -1;
+            goto out_error;
+        }
+        close( fd_from );
+
+        return 0;
+    }
+
+  out_error:
+    saved_errno = errno;
+
+    close( fd_from );
+    if( fd_to >= 0 )
+        close( fd_to );
+
+    errno = saved_errno;
+    return errno;
+}
+
 static ngx_int_t ngx_mod_harbour_handler( ngx_http_request_t * r )
 {
-   void * lib_harbour = dlopen( "./libharbour.so.3.2.0", RTLD_LAZY );
-   
-   lib_harbour = lib_harbour;
+   void * lib_harbour;
+   // unsigned int dwThreadId = pthread_self();
+   char * szTempFileName = "/tmp/libharbour.so.3.2.0";
 
-   return mh_rputs( r, dlerror() ); 
+   CopyFile( "./libharbour.so.3.2.0", szTempFileName, 0 );
+
+   if( ( lib_harbour = dlopen( szTempFileName, RTLD_LAZY ) ) )
+   {   
+      mh_rputs( r, "ok" );
+      dlclose( lib_harbour );
+   }
+   else
+      mh_rputs( r, dlerror() ); 
+
+   return NGX_OK; 
 }
 
 /**
