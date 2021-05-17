3104439a7d2ff34e7553c0bb3daf4b094df73442
FiveTech Software <antonio.fivetech@gmail.com>
2021-05-17 07:54:56 +0200

    Update ngx_mod_harbour.c

--- nginx/ngx_mod_harbour.c
+++ nginx/ngx_mod_harbour.c
@@ -1,47 +1,13 @@
-/**
- * @file   ngx_mod_harbour.c
- * @author António P. P. Almeida <appa@perusio.net>
- * @date   Wed Aug 17 12:06:52 2011
- *
- * @brief  A hello world module for Nginx.
- *
- * @section LICENSE
- *
- * Copyright (C) 2011 by Dominic Fallows, António P. P. Almeida <appa@perusio.net>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-#define HELLO_WORLD "mod_harbour\r\n"
-
 static char * ngx_mod_harbour_main( ngx_conf_t * cf, ngx_command_t * cmd, void * conf );
 static ngx_int_t ngx_mod_harbour_handler(ngx_http_request_t *r);
 
-/**
- * This module provided directive: hello world.
- *
- */
-static ngx_command_t ngx_mod_harbour_commands[] = {
-
-    { ngx_string("mod_harbour"), /* directive */
+static ngx_command_t ngx_mod_harbour_commands[] = 
+{
+    { ngx_string( "mod_harbour" ), /* directive */
       NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS, /* location context and takes
                                             no arguments*/
       ngx_mod_harbour_main, /* configuration setup function */
@@ -52,10 +18,8 @@ static ngx_command_t ngx_mod_harbour_commands[] = {
     ngx_null_command /* command termination */
 };
 
-/* The hello world string. */
-static u_char ngx_hello_world[] = HELLO_WORLD;
+static u_char szMsg[] = "hello world from mod_harbour\r\n";
 
-/* The module context. */
 static ngx_http_module_t ngx_mod_harbour_ctx = {
     NULL, /* preconfiguration */
     NULL, /* postconfiguration */
@@ -86,15 +50,7 @@ ngx_module_t ngx_mod_harbour = {
     NGX_MODULE_V1_PADDING
 };
 
-/**
- * Content handler.
- *
- * @param r
- *   Pointer to the request structure. See http_request.h.
- * @return
- *   The status of the response generation.
- */
-static ngx_int_t ngx_mod_harbour_handler(ngx_http_request_t *r)
+static ngx_int_t ngx_mod_harbour_handler( ngx_http_request_t * r )
 {
     ngx_buf_t *b;
     ngx_chain_t out;
@@ -110,16 +66,16 @@ static ngx_int_t ngx_mod_harbour_handler(ngx_http_request_t *r)
     out.buf = b;
     out.next = NULL; /* just one buffer */
 
-    b->pos = ngx_hello_world; /* first position in memory of the data */
-    b->last = ngx_hello_world + sizeof(ngx_hello_world) - 1; /* last position in memory of the data */
+    b->pos = szMsg; /* first position in memory of the data */
+    b->last = szMsg + sizeof( szMsg ) - 1; /* last position in memory of the data */
     b->memory = 1; /* content is in read-only memory */
     b->last_buf = 1; /* there will be no more buffers in the request */
 
     /* Sending the headers for the reply. */
     r->headers_out.status = NGX_HTTP_OK; /* 200 status code */
     /* Get the content length of the body. */
-    r->headers_out.content_length_n = sizeof(ngx_hello_world) - 1;
-    ngx_http_send_header(r); /* Send the headers */
+    r->headers_out.content_length_n = sizeof( szMsg ) - 1;
+    ngx_http_send_header( r ); /* Send the headers */
 
     /* Send the body, and return the status code of the output filter chain. */
     return ngx_http_output_filter(r, &out);
@@ -146,4 +102,4 @@ static char * ngx_mod_harbour_main( ngx_conf_t *cf, ngx_command_t *cmd, void *co
     clcf->handler = ngx_mod_harbour_handler;
 
     return NGX_CONF_OK;
-} /* ngx_http_hello_world */
+}
