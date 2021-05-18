92fd0cfd412d4df973af3982be07b187898015bc
FiveTech Software <antonio.fivetech@gmail.com>
2021-05-18 17:32:48 +0200

    Create entry.c

---
+++ source/entry.c
@@ -0,0 +1,17 @@
+#include <hbapi.h>
+#include <hbvm.h>
+
+static void * pRequestRec;
+
+HB_EXPORT_ATTR int hb_apache( void * _pRequestRec )
+{
+   pRequestRec = _pRequestRec;
+ 
+   hb_vmInit( HB_TRUE );
+   return hb_vmQuit();
+}   
+
+void * GetRequestRec( void )
+{
+   return pRequestRec;
+}
