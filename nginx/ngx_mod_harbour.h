dbad8cca8ab5fd968c03a5cbb7874c15044aa123
FiveTech Software <antonio.fivetech@gmail.com>
2021-05-19 11:11:48 +0200

    Update ngx_mod_harbour.h

--- nginx/ngx_mod_harbour.h
+++ nginx/ngx_mod_harbour.h
@@ -2,12 +2,15 @@
 
 void * GetNgxApi( void );
 void * GetRequestRec( void );
+char * mh_args( void * r );
 
 typedef int ( * PMH_RPUTS ) ( void * r, const char * szText );
+typedef int ( * PMH_ARGS ) ( void * r );
 
 typedef struct _NGX_API
 {
    PMH_RPUTS mh_rputs;          
+   PMH_ARGS  mh_args;
 } NGX_API;
 
 #endif
