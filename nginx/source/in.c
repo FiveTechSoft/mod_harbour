acde2eb29eec35cb039ee4c5d46b6a2cfe4cee53
FiveTech Software <antonio.fivetech@gmail.com>
2021-05-19 11:16:09 +0200

    Update in.c

--- nginx/source/in.c
+++ nginx/source/in.c
@@ -1,1 +1,13 @@
+#include <ngx_mod_harbour.h>
+#include <hbapi.h>
 
+//----------------------------------------------------------------//
+
+HB_FUNC( MH_ARGS )
+{
+   NGX_API * pNgxApi = GetNgxApi();
+  
+   hb_retc( pNgxApi->mh_args( GetRequestRec() ) );
+}  
+
+//----------------------------------------------------------------//
