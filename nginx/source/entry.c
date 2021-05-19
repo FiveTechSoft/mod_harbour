3e89bf3215a7d4070a9772e927ae0ad4dbc6efe8
FiveTech Software <antonio.fivetech@gmail.com>
2021-05-19 08:51:50 +0200

    Create entry.c

---
+++ nginx/source/entry.c
@@ -0,0 +1,24 @@
+#include <hbapi.h>
+#include <hbvm.h>
+
+static void * pRequestRec;
+static void * pNgxApi;
+
+HB_EXPORT_ATTR int hb_apache( void * _pRequestRec, void * _pNgxApi )
+{
+   pRequestRec = _pRequestRec;
+   pNgxApi = _pNgxApi;
+ 
+   hb_vmInit( HB_TRUE );
+   return hb_vmQuit();
+}   
+
+void * GetRequestRec( void )
+{
+   return pRequestRec;
+}
+
+void * GetNgxApi( void )
+{
+   return pNgxApi;
+}  
