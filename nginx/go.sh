db256cf09bc7d62ecc313987d6b15c2918b6d805
FiveTech Software <antonio.fivetech@gmail.com>
2021-05-21 08:37:54 +0200

    Update go.sh

--- nginx/go.sh
+++ nginx/go.sh
@@ -1,6 +1,6 @@
 ../../harbour/bin/linux/gcc/hbmk2 modharbour.hbp
 mv liblibharbour.so libharbour.so
-sudo cp libharbour.so /usr/local/nginx/modules/libharbour.so
+sudo cp ./libharbour.so /usr/local/nginx/modules/libharbour.so
 cd ../../nginx/objs
 ./nginx -s quit
 ./nginx
