765b135aff58ffdb517e6a1570976876ba272631
FiveTech Software <antonio.fivetech@gmail.com>
2021-05-21 09:40:01 +0200

    Update go.sh

--- nginx/go.sh
+++ nginx/go.sh
@@ -3,6 +3,6 @@ mv liblibharbour.so libharbour.so
 sudo cp ./libharbour.so /usr/local/nginx/modules/libharbour.so
 sudo cp ./libharbour.so ../../nginx/objs/libharbour.so
 cd ../../nginx/objs
-./nginx -s quit
-./nginx
+sudo ./nginx -s quit
+sudo ./nginx
 curl http://localhost?test.prg
