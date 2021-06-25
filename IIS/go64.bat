8de1159e285cbbcecb356881b718037eae7b7e61
Antonio Linares <antonio.fivetech@gmail.com>
2021-06-25 16:33:10 +0200

    removed unneeded libs

--- IIS/go64.bat
+++ IIS/go64.bat
@@ -2,6 +2,6 @@
 @set oldinclude=%INCLUDE%
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 msbuild msvc\mod_harbour.sln /p:Configuration=Release
-c:\harbour\bin\win\msvc64\hbmk2 modharbour.hbp -comp=msvc64 -Lc:\Apache24\lib
+c:\harbour\bin\win\msvc64\hbmk2 modharbour.hbp -comp=msvc64
 @set Path=%oldpath%
 @set INCLUDE=%oldinclude%
