9c82c0bcbc5e05c11f4fa86e0b47a28abfe29635
Antonio Linares <antonio.fivetech@gmail.com>
2021-09-19 09:31:23 +0200

    clean before build

--- IIS/go64.bat
+++ IIS/go64.bat
@@ -1,7 +1,7 @@
 @set oldpath=%Path%
 @set oldinclude=%INCLUDE%
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-msbuild msvc\mod_harbour.sln /p:Configuration=Release
+msbuild msvc\mod_harbour.sln /p:Configuration=Release /t:Clean,Build
 c:\harbour\bin\win\msvc64\hbmk2 modharbour.hbp -comp=msvc64
 @set Path=%oldpath%
 @set INCLUDE=%oldinclude%
--- IIS/go64ads.bat
+++ IIS/go64ads.bat
@@ -1,6 +1,7 @@
 @set oldpath=%path%
 @set oldinclude=%INCLUDE%
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+msbuild msvc\mod_harbour.sln /p:Configuration=Release /t:Clean,Build
 c:\harbour\bin\win\msvc64\hbmk2 modharbour.hbp -comp=msvc64 -dHB_WITH_ADS="c:\Program Files\Advantage 11.10\acesdk\" rddads.hbc
 @set path=%oldpath%
 @set INCLUDE=%oldinclude%
